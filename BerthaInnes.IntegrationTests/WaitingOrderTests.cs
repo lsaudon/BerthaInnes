f28304e66f83f7f95cc87ae7a5d7472cd137ff40
lsaudon <lsaudon@hotmail.com>
2020-04-10 10:21:27 +0200

    Add integration test

--- BerthaInnes.IntegrationTests/WaitingOrderTests.cs
+++ BerthaInnes.IntegrationTests/WaitingOrderTests.cs
@@ -1,4 +1,9 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
+using BerthaInnes.CommandSide;
+using BerthaInnes.CommandSide.DomainCommands;
+using BerthaInnes.CommandSide.DomainEvents;
+using BerthaInnes.QuerySide;
 using Xunit;
 
 namespace BerthaInnes.IntegrationTests
@@ -8,7 +13,22 @@ namespace BerthaInnes.IntegrationTests
         [Fact]
         public void Should_Display_Updated_Projection_When_Send_Command()
         {
+            var repository = new List<WaitingOrder>();
+            var pendingOrderEventHandler = new PendingOrderEventHandler(repository);
+            var eventHandlers = new List<IEventHandler> { pendingOrderEventHandler };
 
+            var eventStore = new List<EventWrapper>();
+            var pubSub = new PubSub(eventStore,eventHandlers);
+
+            var colisList = new List<Colis> { new Colis() };
+            var domainEvents = Order.Decide(new StartOrder(colisList), new List<IDomainEvent>()).ToList();
+
+            foreach (var domainEvent in domainEvents)
+            {
+                pubSub.Publish(new EventWrapper("1",domainEvent));
+            }
+            
+            Assert.Single(repository);
         }
     }
 }
